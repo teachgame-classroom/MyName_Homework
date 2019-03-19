ab472e1fe5354aa44004800a4e37fc114af541cb
teachgame <teachgame@163.com>
2019-03-19 16:33:08 +0800

    作业 190319

---
+++ Homework_190319/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Homework_190319
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("这个人很懒，什么作业也没做");
+            Console.ReadKey();
+        }
+    }
+}
