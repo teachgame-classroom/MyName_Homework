14316522a3d704adf05835cec2aa745cdfa2609f
teachgame <teachgame@163.com>
2019-03-20 15:23:56 +0800

    参考答案 19.03.20 by HeZhiPeng #1

--- Homework_190319/Program.cs
+++ Homework_190319/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Homework_190319
 {
@@ -9,8 +10,16 @@ namespace Homework_190319
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("这个人很懒，什么作业也没做");
-            Console.ReadKey();
+            Student xiaolang = new Student("小狼", "男", 17, 95, 95, 90);
+
+            xiaolang.SayHello();
+            xiaolang.PrintScore();
+
+            Student s1 = new Student("小狼1", "男", 17, 60, 50, 40);
+            s1.PrintScore();
+
+            Student s2 = new Student("小狼2", "男", 17, 75, 88, 85);
+            s2.PrintScore();
         }
     }
 }
